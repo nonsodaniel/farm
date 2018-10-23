3ef04e1adda19f1712ae67af97b6b9893613a4e6
Nonso <nonsodaniel07@gmail.com>
2018-10-23 13:53:30 +0100

    third commit

--- js/app.js
+++ js/app.js
@@ -9,7 +9,7 @@ var farm = function(){
 		actions: function($el, action_type){
 			var method_ = {
 				loadHistory: function(){
-					
+					alert("Hello you")
 				},
 				loadLogins : function(){
 					
@@ -52,34 +52,37 @@ var farm = function(){
 
 		event: function(){
 
-				$('body').on("click",".loadHistory", function(){
-					
-
-				})
-
-
-				$('body').on("click",".loadLogins", function(){
-				
-
-				})
-
-				$('body').on("click",".loadProductStockIn", function(){
-					
-
-				})
-
-
-
-
-
-
-
+			
 				$('body #farm_cont').on("click","[data-action]", function(){
 				
 				})
 
-				$(".middleCollection").on("click", function(){
-					
+				$(".sign_up").on("click", function(){
+					farm().loadHistory()
+					var name = $(".name").val();
+					var email = $(".email").val();
+					var phone = $(".phone").val();
+					var state = $(".state").val();
+					var lga = $(".lga").val();
+					var password1 = $(".password1").val();
+					var password2 = $(".password2").val();
+
+					if(password1 != password2){
+						alert("passwords do not match")
+					}
+					console.log(name, email, phone, state, lga);
+					var arr = [], obj = {};
+					arr.push(name, email, phone, state, lga)
+					obj.myname = name;
+					obj.my_email = email;
+					obj.my_phone = phone;
+					obj.my_state = state;
+					obj.my_lga = lga;
+					console.log(arr, obj);
+
+
+
+
 				})
 
 				$(".historyPerPeriod").on("click", function(){
